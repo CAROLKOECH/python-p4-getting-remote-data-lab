2144ceeaa64fd7e3ea1db5050b682804ca5bdef7
CAROLKOECH <koechc44@gmail.com>
2023-09-28 10:57:13 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,17 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            return json.loads(response_body)
+        except json.JSONDecodeError:
+            print(f"Error decoding JSON data from {self.url}")
+            return None
